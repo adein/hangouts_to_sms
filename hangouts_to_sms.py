db26e561ab73a15e0634b4319e81362d4f57a9e9
Andrew Somerville <asomerville@carnegierobotics.com>
2017-05-20 22:37:49 -0400

    convert hardcoded parameters to argparse instead

--- hangouts_to_sms.py
+++ hangouts_to_sms.py
@@ -1,19 +1,32 @@
+#!/usr/bin/python3
+
+import sys
+import argparse
+
 from hangouts_parser import HangoutsParser
 from titanium_backup_formatter import TitaniumBackupFormatter
 
+def main():
+    parser = argparse.ArgumentParser(description="Run logs on subrepos recursively")
+    parser.add_argument("-ph", "--phonenumber", type=str, help="your phone number", required=True)
+    parser.add_argument("-of", "--outfile", type=str, help="file to write to", default="messages.xml")
+    parser.add_argument("-if", "--infile", type=str, help="file to read from", default="Hangouts.json")
+    args = parser.parse_args()
 
-# Configuration constants
-HANGOUTS_JSON_FILE = 'Hangouts.json'
-OUTPUT_FILE = "messages.xml"
-YOUR_PHONE_NUMBER = "+11234567890"
+    # Parse the Hangouts data and output Titanium Backup XML
+    print("Parsing Hangouts data file...")
+    hangouts_parser = HangoutsParser()
+    conversations, self_gaia_id = hangouts_parser.parse_input_file(args.infile, args.phonenumber)
+    print("Done.")
+
+    print("Converting to SMS export file...")
+    titanium_output = TitaniumBackupFormatter()
+    titanium_output.create_output_file(conversations, self_gaia_id, args.outfile)
+    print("Done.")
+
+    return 0
 
 
-# Parse the Hangouts data and output Titanium Backup XML
-hangouts_parser = HangoutsParser()
-titanium_output = TitaniumBackupFormatter()
-print("Parsing Hangouts data file...")
-conversations, self_gaia_id = hangouts_parser.parse_input_file(HANGOUTS_JSON_FILE, YOUR_PHONE_NUMBER)
-print("Done.")
-print("Converting to SMS export file...")
-titanium_output.create_output_file(conversations, self_gaia_id, OUTPUT_FILE)
-print("Done.")
+if __name__ == '__main__':
+    sys.exit(main())
+
